30c24b42cb7de46926c79507e083c426e1fc003d
sidney-wilke <mail@sidneywilke.com>
2024-05-14 16:58:58 +0200

    changes for 3 windows

--- app/dashboard/callbacks.py
+++ app/dashboard/callbacks.py
@@ -5,60 +5,36 @@ from ..model_manager import ModelManager
 
 model_manager = ModelManager()
 
-def register_callbacks(dash_app):
-    @dash_app.callback(
-        Output('interval-component', 'disabled'),
-        Input('submit-button', 'n_clicks'),
-        State('input-text', 'value')
-    )
-    def trigger_streaming(n_clicks, input_value):
-        global stream_generator
-        if n_clicks > 0:
-            stream_generator = model_manager.get_response(input_value)  # Starten des Generators
-            return False  # Aktiviert das Interval
-        return True
-
-    @dash_app.callback(
-        Output('output-container', 'children'),
-        Input('interval-component', 'n_intervals'),
-        State('output-container', 'children')
-    )
-    def update_output_div(n, existing_text):
-        try:
-            new_text = next(stream_generator)  # Holt das nächste Wort
-            return existing_text + new_text if existing_text else new_text
-        except StopIteration:
-            return existing_text  # Stoppt die Aktualisierung, wenn alle Worte abgerufen wurden
-
-
 from dash.dependencies import Input, Output, State
 import requests
 
 stream_generator_1 = None
 stream_generator_2 = None
+stream_generator_3 = None
 
 def register_callbacks(dash_app):
     @dash_app.callback(
-        [Output('interval-component-1', 'disabled'), Output('interval-component-2', 'disabled')],
+        [Output('interval-component-1', 'disabled'), Output('interval-component-2', 'disabled'), Output('interval-component-3', 'disabled')],
         Input('submit-button', 'n_clicks'),
-        [State('input-text', 'value'), State('model-dropdown-1', 'value'), State('model-dropdown-2', 'value')]
+        [State('input-text', 'value'), State('model-dropdown-1', 'value'), State('model-dropdown-2', 'value'), State('model-dropdown-3', 'value')]
     )
-    def trigger_streaming(n_clicks, input_value, model1, model2):
-        global stream_generator_1, stream_generator_2
+    def trigger_streaming(n_clicks, input_value, model1, model2, model3):
+        global stream_generator_1, stream_generator_2, stream_generator_3
         if n_clicks > 0:
             stream_generator_1 = model_manager.get_response(input_value, model1)
-            stream_generator_2 = model_manager.get_response(input_value, model2)  # Starten des Generators
-            return False, False  # Aktiviert beide Intervals
-        return True, True
+            stream_generator_2 = model_manager.get_response(input_value, model2)
+            stream_generator_3 = model_manager.get_response(input_value, model3) # Starten des Generators
+            return False, False, False  # Aktiviert beide Intervals
+        return True, True, True
 
     @dash_app.callback(
-        [Output('output-container-1', 'children'), Output('output-container-2', 'children')],
-        [Input('interval-component-1', 'n_intervals'), Input('interval-component-2', 'n_intervals')],
-        [State('output-container-1', 'children'), State('output-container-2', 'children')]
+        [Output('output-container-1', 'children'), Output('output-container-2', 'children'), Output('output-container-3', 'children')],
+        [Input('interval-component-1', 'n_intervals'), Input('interval-component-2', 'n_intervals'), Input('interval-component-3', 'n_intervals')],
+        [State('output-container-1', 'children'), State('output-container-2', 'children'), State('output-container-3', 'children')]
     )
-    def update_output_div(n1, n2, existing_text_1, existing_text_2):
-        global stream_generator_1, stream_generator_2
-        new_text_1, new_text_2 = existing_text_1, existing_text_2
+    def update_output_div(n1, n2, n3, existing_text_1, existing_text_2, existing_text_3):
+        global stream_generator_1, stream_generator_2, stream_generator_3
+        new_text_1, new_text_2, new_text_3 = existing_text_1, existing_text_2, existing_text_3
         if n1 is not None:
             try:
                 new_text_1 += next(stream_generator_1)
@@ -69,4 +45,9 @@ def register_callbacks(dash_app):
                 new_text_2 += next(stream_generator_2)
             except StopIteration:
                 pass
-        return new_text_1, new_text_2
+        if n3 is not None:
+            try:
+                new_text_3 += next(stream_generator_3)
+            except StopIteration:
+                pass
+        return new_text_1, new_text_2, new_text_3
--- app/dashboard/layout.py
+++ app/dashboard/layout.py
@@ -37,18 +37,32 @@ def init_dashboard(server):
                     value='mistral',
                     style={'width': '25%', 'display': 'inline-block', 'margin-left': '5px'}
                 ),
+                dcc.Dropdown(
+                    id='model-dropdown-3',
+                    options=[
+                        {'label': 'Phi3', 'value': 'phi3'},
+                        {'label': 'Mistral', 'value': 'mistral'},
+                        {'label': 'Gemma', 'value': 'gemma'}
+                    ],
+                    value='mistral',
+                    style={'width': '25%', 'display': 'inline-block', 'margin-left': '5px'}
+                ),
                 html.Button('Submit', id='submit-button', n_clicks=0, className="btn btn-primary ml-2")
-            ], width=6)
+            ], width=4),
         ]),
         dbc.Row([
             dbc.Col([
                 html.Div(id='output-container-1', children='Response from Model 1 will appear here.'),
                 dcc.Interval(id='interval-component-1', interval=1000, n_intervals=0, disabled=True)
-            ], width=6),
+            ], width=4),
             dbc.Col([
                 html.Div(id='output-container-2', children='Response from Model 2 will appear here.'),
                 dcc.Interval(id='interval-component-2', interval=1000, n_intervals=0, disabled=True)
-            ], width=6)
+            ], width=4),
+            dbc.Col([
+                html.Div(id='output-container-3', children='Response from Model 3will appear here.'),
+                dcc.Interval(id='interval-component-3', interval=1000, n_intervals=0, disabled=True)
+            ], width=4)
         ])
     ], fluid=True)
 
